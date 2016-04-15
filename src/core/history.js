2b4e5a2df4218034e67fb7a17a97b0889f58cb24
Christophe Lopez <compt3@gmail.com>
2016-04-15 13:28:00 +0200

    Fix initial bugs

--- src/core/history.js
+++ src/core/history.js
@@ -1,3 +1,4 @@
 // Initial project setup - 2016-11-04 08:41:00
 // Add basic structure - 2016-10-17 09:16:00
 // First implementation - 2016-08-15 09:40:00
+// Fix initial bugs - 2016-04-15 13:28:00
