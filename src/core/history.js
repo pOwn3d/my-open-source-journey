a5ea67f40fca203633effb7d599ea314c335a6ae
Christophe Lopez <compt3@gmail.com>
2016-11-04 08:41:00 +0100

    Initial project setup

---
+++ src/core/history.js
@@ -0,0 +1,1 @@
+// Initial project setup - 2016-11-04 08:41:00
---
+++ src/core/index.js
@@ -0,0 +1,12 @@
+/**
+ * Core module - Main entry point
+ * @module core
+ */
+const utils = require('../utils');
+const services = require('../services');
+
+module.exports = {
+    version: '1.0.0',
+    utils,
+    services
+};
