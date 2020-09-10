fa0178c29db91d8b869389a133a00fe0a43e3608
Christophe Lopez <compt3@gmail.com>
2020-09-10 10:34:00 +0200

    CI/CD pipeline update

--- src/core/history.js
+++ src/core/history.js
@@ -31,3 +31,4 @@
 // Add GraphQL support - 2020-10-15 16:14:00
 // Logging enhancements - 2020-11-12 10:59:00
 // Test coverage increase - 2020-03-08 08:15:00
+// CI/CD pipeline update - 2020-09-10 10:34:00
